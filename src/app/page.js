7f38f2473195178887543820d2afec3f50a79aa6
Anna Ravna <ansvravna@gmail.com>
2024-12-10 12:46:40 +0100

    file structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from './components/Header';
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <Header />
         <Image
           className={styles.logo}
           src="/next.svg"
